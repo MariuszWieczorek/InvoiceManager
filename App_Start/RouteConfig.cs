de848b91773cc9e491912272659ccc2bfc23d607
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2021-01-10 23:57:00 +0100

    Lekcja 16,  40 minuta wyświetla się lista faktur wyświetla się ekran dodawania faktury, bez widoku częściowego dokumentacja do tego momentu

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,13 +13,7 @@ namespace InvoiceManager
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "My",
-                url: "test/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-
-
+    
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using InvoiceManager.Models.Domains;
+using InvoiceManager.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,52 @@ namespace InvoiceManager.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var invoices = new List<Invoice>
+            {
+                new Invoice
+                {
+                    Id = 1,
+                    Title = "FA/01/2021",
+                    Value = 5320,
+                    CreatedDate = DateTime.Now,
+                    PaymentDate = DateTime.Now,
+                    Client = new Client { Name = "Klient1" }
+                },
+
+                new Invoice
+                {
+                    Id = 2,
+                    Title = "FA/02/2021",
+                    Value = 120,
+                    CreatedDate = DateTime.Now,
+                    PaymentDate = DateTime.Now,
+                    Client = new Client { Name = "Klient2" }
+                }
+            };
+            
+            return View(invoices);
+        }
+
+        public ActionResult Invoice(int id = 0)
+        {
+            var vm = new EditInvoiceViewModel
+            {
+                Clients = new List<Client>
+                {
+                    new Client {Id = 1, Name = "Klient1"}
+                },
+
+                MethodOfPayments = new List<MethodOfPayment>
+                {
+                    new MethodOfPayment {Id = 1, Name = "Przelew" }
+                },
+
+                Heading = "Edycja Faktury 1",
+
+                Invoice = new Invoice()
+
+            };
+            return View(vm); 
         }
 
         [AllowAnonymous]
