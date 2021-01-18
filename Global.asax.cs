6352249b54bc8a19b06f57ac12c1ffada717842e
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2021-01-18 23:56:26 +0100

    walidacja, prawie koniec

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,18 @@ namespace InvoiceManager
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        // metoda wywo³ywana przed ka¿dym ¿¹daniem
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            CultureInfo newCulture = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+            newCulture.DateTimeFormat.ShortDatePattern = "dd-MM-yyyy";
+            newCulture.DateTimeFormat.DateSeparator = "-";
+            newCulture.NumberFormat.NumberDecimalSeparator = ",";
+            newCulture.NumberFormat.NumberDecimalDigits = 2;
+            newCulture.NumberFormat.NumberGroupSeparator = "";
+            Thread.CurrentThread.CurrentCulture = newCulture;
         }
     }
 }
