ac09daaf9792a0e21e0312518d2513dc0876c214
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2021-01-22 19:05:32 +0100

    nieudana próba zapisu do pdf

--- Controllers/PrintController.cs
+++ Controllers/PrintController.cs
@@ -1,10 +1,11 @@
-﻿using InvoiceManager.Models.Repositories;
+﻿using InvoiceManager.Models.Domains;
+using InvoiceManager.Models.Repositories;
 using Microsoft.AspNet.Identity;
+using Rotativa;
+using Rotativa.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+
 
 namespace InvoiceManager.Controllers
 {
@@ -14,12 +15,53 @@ namespace InvoiceManager.Controllers
         private InvoiceRepository _invoiceRepository = new InvoiceRepository();
         public ActionResult InvoiceTemplate(int id)
         {
-
-
             var userId = User.Identity.GetUserId();
             var invoice = _invoiceRepository.GetInvoice(id,userId);
 
             return View(invoice);
         }
+        
+        [HttpPost]
+        public ActionResult InvoiceToPdf(int id)
+        {
+            var handle = Guid.NewGuid().ToString();
+            var userId = User.Identity.GetUserId();
+            var invoice = _invoiceRepository.GetInvoice(id, userId);
+            
+            TempData[handle] = GetPdfContent(invoice);
+
+            System.IO.File.AppendAllText(@"d:\InvoiceToPdf.txt", $"{invoice.Title} {handle} \n ");
+            
+            return Json(new
+            {
+                Success = true,
+                FileGuid = handle,
+                FileName = $@"Faktura_{invoice.Id}.pdf"
+            });
+        }
+
+        private byte[] GetPdfContent(Invoice invoice)
+        {
+            var pdfResult = new ViewAsPdf(@"InvoiceTemplate", invoice)
+            {
+                PageSize = Size.A4,
+                PageOrientation = Orientation.Portrait
+            };
+
+            return pdfResult.BuildFile(ControllerContext);
+        }
+
+        public ActionResult DownloadInvoicePdf(string fileGuid, string fileName)
+        {
+            System.IO.File.AppendAllText(@"d:\DownloadInvoicePdf.txt", $"{fileGuid} {fileName} \n ");
+
+            if (TempData[fileGuid] == null)
+                throw new Exception("Błąd przy próbie eksportu faktury do PDF.");
+
+            var data = TempData[fileGuid] as byte[];
+
+            return File(data, "application/pdf", fileName);
+        }
+
     }
 }
